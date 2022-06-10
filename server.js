e7b1f04b645a8cdda860594b24fc2b6e053a9016
ufombam <ufombam@gmail.com>
2022-06-10 10:34:09 +0100

    fixing http OPTIONS

--- server.js
+++ server.js
@@ -31,14 +31,19 @@ const db = knex({
 });
 const app = express();
 app.use(express.json());
-app.use(cors());
-app.use((req, res, next) => {
+//app.use(cors());
+app.use(function(req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    // handle OPTIONS method
     if ('OPTIONS' == req.method) {
-        return res.status(200);
+        return res.sendStatus(200);
     } else {
         next();
     }
-})
+});
 
 
 app.get('/', (req, res) => res.json('App is working'))
